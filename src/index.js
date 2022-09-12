c5f402c728cecc4cdeaa853d0cc915926d6506b0
Golan Blumenkrantz <golan281@gmail.com>
2022-09-12 23:54:15 +0300

    helmet fix attempt 2.0

--- src/index.js
+++ src/index.js
@@ -8,11 +8,11 @@ import { HelmetProvider } from 'react-helmet-async';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <HelmetProvider>
-          <App />
-      </HelmetProvider>
-    </BrowserRouter>
+    <HelmetProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </HelmetProvider>
   </React.StrictMode >
 );
 
